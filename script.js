9ac8e7e09268410641e5283649c7d77a5ce38476
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-02-07 20:40:28 -0500

    Created function that plays a round of a game
    This function takes both the users input and the computer's choice
    to determine who wins the rounds and return a string that exclaims
    the winner.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function getComputerChoice () {
     let choice = Math.floor(Math.random() * 3);
-    console.log(choice);
     if (choice === 0) {
         return 'Rock';
     } else if (choice === 1) {
@@ -9,3 +8,26 @@ function getComputerChoice () {
         return 'Scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+    let computerChoice = computerSelection.toLowerCase();
+
+    if (playerChoice === computerChoice) {
+        return "It's a Tie! Both chose " + playerChoice;
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        return "You Lose! Paper beats Rock.";
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        return "You Win! Rock beats Scissors.";
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        return "You Win! Paper beats Rock.";
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        return "You Lose! Scissors beats Paper.";
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        return "You Lose! Rock beats Scissors.";
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        return "You Win! Scissors beats Paper";
+    }
+}
+
+console.log(playRound("Rock", getComputerChoice()));
