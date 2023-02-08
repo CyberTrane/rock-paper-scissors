ef2b8b4f510e0f2a8e9772303b50a5894462beae
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-02-07 20:14:55 -0500

    Capitalized choices in computer choice function

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function getComputerChoice () {
     let choice = Math.floor(Math.random() * 3);
     console.log(choice);
     if (choice === 0) {
-        return 'rock';
+        return 'Rock';
     } else if (choice === 1) {
-        return 'paper';
+        return 'Paper';
     } else {
-        return 'scissors';
+        return 'Scissors';
     }
 }
