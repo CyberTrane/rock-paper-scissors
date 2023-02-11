411489c599a8bd944b1eddaada6ed81af574c88e
CyberTrane <58540182+CyberTrane@users.noreply.github.com>
2023-02-10 23:05:55 -0500

    Added buttons with listeners, div shows results

--- script.js
+++ script.js
@@ -9,33 +9,55 @@ function getComputerChoice () {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    let playerChoice = playerSelection.toLowerCase();
-    let computerChoice = computerSelection.toLowerCase();
+function playRound(e) {
+    div.textContent = '';
+    let playerChoice = this.textContent.toLowerCase();
+    let computerChoice = getComputerChoice().toLowerCase();
 
     if (playerChoice === computerChoice) {
-        return "It's a Tie! Both chose " + playerChoice;
+        div.textContent = "It's a Tie! Both chose " + playerChoice;
     } else if (playerChoice === "rock" && computerChoice === "paper") {
-        return "You Lose! Paper beats Rock.";
+        div.textContent = "You Lose! Paper beats Rock.";
     } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        return "You Win! Rock beats Scissors.";
+        div.textContent = "You Win! Rock beats Scissors.";
     } else if (playerChoice === "paper" && computerChoice === "rock") {
-        return "You Win! Paper beats Rock.";
+        div.textContent = "You Win! Paper beats Rock.";
     } else if (playerChoice === "paper" && computerChoice === "scissors") {
-        return "You Lose! Scissors beats Paper.";
+        div.textContent = "You Lose! Scissors beats Paper.";
     } else if (playerChoice === "scissors" && computerChoice === "rock") {
-        return "You Lose! Rock beats Scissors.";
+        div.textContent = "You Lose! Rock beats Scissors.";
     } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        return "You Win! Scissors beats Paper";
+        div.textContent = "You Win! Scissors beats Paper.";
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let userChoice = prompt("Choose Rock, Paper, or Scissors");
-        let compChoice = getComputerChoice();
-        console.log(playRound(userChoice, compChoice));
-    }
-}
+const rockButton = document.createElement('button');
+rockButton.textContent = 'Rock';
+const paperButton = document.createElement('button');
+paperButton.textContent = 'Paper';
+const scissorsButton = document.createElement('button');
+scissorsButton.textContent = 'Scissors';
 
-game();
+const bod = document.querySelector('body');
+bod.appendChild(rockButton);
+bod.appendChild(paperButton);
+bod.appendChild(scissorsButton);
+
+const buttons = document.querySelectorAll('button');
+
+const div = document.createElement('div');
+bod.appendChild(div);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', playRound);
+});
+
+/* function game() { */
+
+/* let userChoice = prompt("Choose Rock, Paper, or Scissors");
+let compChoice = getComputerChoice();
+console.log(playRound(userChoice, compChoice)); */
+
+/* }
+
+game(); */
